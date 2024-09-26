8f1e21a00ce0ba2a39296ca3b68f9e585d9b72b8
moopyfloop <moopyfloop8@gmail.com>
2024-09-26 23:43:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-map.locate({setView: true, maxZoom: 16});
-
-document.getElementById("map").style.display = "none";
-
 function mboxFunction() {
     var mbox = document.getElementById("mbox");
     mbox.classList.toggle("expandmbox");
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const areas = document.querySelectorAll('.land');
-    const info = document.getElementById('info');
-    areas.forEach(area => {
-        area.addEventListener('click', () => {
-            info.textContent = `You clicked on ${area.getAttribute('data-name')}`;
-            document.getElementById("map").style.display = "block";
-        });
-    });
-});
